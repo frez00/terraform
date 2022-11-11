78dbf77256b95a7473f3e84d0d0415499ae73c18
frez00 <tahathewind@gmail.com>
2022-11-11 15:51:26 -0800

    Staticwebsite

---
+++ S3-staticwebsite.tf
@@ -0,0 +1,30 @@
+resource "aws_s3_bucket" "tahastaticwebsite1122" {
+    bucket = "tahasstaticwebsiteinterraform1122"
+}
+
+resource "aws_s3_bucket_object" "index" {
+    bucket = aws_s3_bucket.tahastaticwebsite1122.id
+    key    = "index.html"
+    acl    = "public-read"
+    source = "./index.html"
+    content_type = "text/html"
+}
+
+resource "aws_s3_bucket_object" "error" {
+    bucket = aws_s3_bucket.tahastaticwebsite1122.id
+    key    = "error.html"
+    acl    = "public-read"
+    source = "./error.html"
+    content_type = "text/html"
+}
+resource "aws_s3_bucket_website_configuration" "tahawebconfig" {
+    bucket = aws_s3_bucket.tahastaticwebsite1122.bucket
+
+    index_document {
+        suffix = "index.html"
+    }
+
+    error_document {
+        key = "error.html"
+    }
+}
